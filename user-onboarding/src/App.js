17de0f8e16ea0f6908a50cdc43d20d9dec293887
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-12-15 14:26:49 -0600

    finished form, working on app.js

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -3,27 +3,96 @@ import './App.css';
 import Form from './Form.js'; 
 import axios from 'axios'; 
 import * as yup from 'yup'; 
-import React from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 
-function App() {
+const initialFormValues = {
+  name: '', 
+  email: '', 
+  password: '', 
+  termsOfService: false, 
+}
+
+const initialFormErrors = {
+  name: '', 
+  email: '', 
+  password: '', 
+  termsOfService: '',
+}
+
+const initalNewUsers = [];
+const initialDisabled = true;
+
+
+export default function App() {
+
+  const [newUsers, setNewUsers] = useState(initialNewUsers)
+  const [disabled, setDisabled] = useState(initialDisabled)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const getUsers = () => {
+    axios.get('https://reqres.in/api/users')
+    .then(resp => {
+      setNewUsers(resp.data); 
+    }).catch(err => console.error(err))
+  }
+
+  const postNewUsers = newUsers => {
+    axios.post('https://reqres.in/api/users')
+    .then(resp => {
+      setNewUsers(resp.data);
+    }).catch(err => console.error(err))
+    .finally(() => setFormValues(initialFormValues))
+  }
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+    .validate(value)
+    .then(() => setFormErrors({ ...formErrors, [name]: ''}))
+    .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value); 
+    setFormValues({ ...formValues, [name]: value})
+  }
+
+  const formSubmit = () => {
+    const newUsers = {
+      name: formValues.name.trim(), 
+      email: formValues.email.trim(), 
+      password: formValues.password.trim(), 
+      termsOfService: formValues.termsOfService.trim()
+    }
+    postNewUsers(newUsers); 
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header><h1>New Users</h1></header>
+
+      <Form 
+      values={formValues}
+      change={inputChange}
+      submit={formSubmit}
+      disabled={disabled}
+      errors={formErrors}
+      />
+
+
+
+
+
     </div>
   );
 }
 
-export default App;
+
